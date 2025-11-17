ee081aad7d3cec32d814162c97bf426ab8974c53
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 20:33:48 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
+    }
   }
 }
 
