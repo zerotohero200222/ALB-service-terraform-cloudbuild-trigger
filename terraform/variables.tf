f7e65f5bf2e2304ce969996b6cc757a5f99b1b7a
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 19:00:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,26 @@
 variable "project_id" {
-  description = "GCP project where the Cloud Build trigger lives"
-  type        = string
+  type = string
 }
 
 variable "trigger_name" {
-  description = "Name of the Cloud Build trigger"
-  type        = string
+  type = string
 }
 
-variable "github_owner" {
-  description = "GitHub organization or user name"
+variable "repo_name" {
+  description = "Full Cloud Build repository identifier"
   type        = string
-}
-
-variable "github_repo" {
-  description = "GitHub repository name (without owner)"
-  type        = string
-}
-
-variable "branch_regex" {
-  description = "Regex for branch that triggers the build (e.g. ^main$)"
-  type        = string
-  default     = "^main$"
 }
 
 variable "cloudbuild_yaml_path" {
-  description = "Path to cloudbuild config in the repo"
-  type        = string
-  default     = "cloudbuild.yaml"
+  type    = string
+  default = "cloudbuild.yaml"
 }
 
 variable "environment" {
-  description = "Environment label (dev/uat/prod)"
-  type        = string
+  type = string
 }
 
 variable "trigger_service_account" {
-  description = "Service account email used to execute the build"
-  type        = string
+  type = string
 }
+
