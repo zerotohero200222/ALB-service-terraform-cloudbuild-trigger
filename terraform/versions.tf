645b8fcd639d9a50c37259484a17ac1820e3ac66
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 18:43:51 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.3.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
       version = "~> 5.0"
     }
   }
