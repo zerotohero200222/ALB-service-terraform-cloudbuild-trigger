0604e1f696b79fa8ebb3c28adb59c760b06876c9
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 18:59:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,19 @@
 resource "google_cloudbuild_trigger" "pr_trigger" {
   project     = var.project_id
   name        = var.trigger_name
-  description = "Cloud Build GitHub App trigger managed by Terraform"
+  description = "Cloud Build Repo trigger (Terraform managed)"
+  location    = "us-central1"
 
-  # GitHub App trigger (not Cloud Build Repository v2)
-  github {
-    owner = var.github_owner
-    name  = var.github_repo
-
-    push {
-      branch = var.branch_regex
-    }
+  trigger_template {
+    repo_name   = var.repo_name
+    branch_name = "main"
   }
 
-  # Use the cloudbuild.yaml in this repo
   filename = var.cloudbuild_yaml_path
 
-  # Pass environment into the build as a substitution
   substitutions = {
     "_ENV" = var.environment
   }
 
-  # Service account used by Cloud Build to run the steps
   service_account = var.trigger_service_account
 }
