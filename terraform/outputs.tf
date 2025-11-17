2100d2d5e96aec4e8da9a3898ecce9dd50499a14
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 15:09:40 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "pr_trigger_name" {
+  value = var.pr_trigger_name
+}
+
+output "repository" {
+  value = var.repository_name
+}
+
+output "trigger_service_account" {
+  value = var.trigger_service_account
+}
