378c8c8bc54f92884387a90c017edf0437cac55f
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 16:58:22 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,8 @@
-output "pr_trigger_id" {
-  description = "ID of the created Cloud Build PR trigger"
-  value       = google_cloudbuild_trigger.pr_trigger.id
+output "pr_trigger_name" {
+  value = google_cloudbuild_trigger.pr_trigger.name
 }
 
-output "pr_trigger_name" {
-  description = "Name of the created Cloud Build PR trigger"
-  value       = google_cloudbuild_trigger.pr_trigger.name
+output "pr_trigger_id" {
+  value = google_cloudbuild_trigger.pr_trigger.trigger_id
 }
+
