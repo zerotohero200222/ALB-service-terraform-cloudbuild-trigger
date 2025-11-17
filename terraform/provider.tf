b8390470af4e2458d7beb5b6bbc0663526173813
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 16:23:49 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,15 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
 }
 
-provider "google-beta" {
+provider "google" {
   project = var.project_id
   region  = var.region
 }
