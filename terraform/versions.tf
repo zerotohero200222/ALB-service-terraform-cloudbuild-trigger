3aa6cfd74405bc1b670fcc4440fa0b3c05a07e79
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 16:28:24 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.0"
-    }
-  }
-}
+# versions.tf intentionally left blank.
+# Provider configuration moved into provider.tf
+
