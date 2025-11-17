91720e36a2b9767514274f16c7d501458b03f590
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 16:56:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-resource "google_cloudbuild_trigger" "github_push_trigger" {
+resource "google_cloudbuild_trigger" "pr_trigger" {
   project     = var.project_id
-  location    = "us-central1"
   name        = var.trigger_name
-  description = "Cloud Build GitHub App trigger for main branch"
+  description = "Cloud Build GitHub App trigger"
+  location    = "us-central1"
 
   github {
     owner = var.github_owner
