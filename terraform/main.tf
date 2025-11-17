da5a380cff26946a1d5e4c76f5bc3bbb674ec86b
zerotohero200222 <aebharath17@gmail.com>
2025-11-17 18:45:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,26 @@
 resource "google_cloudbuild_trigger" "pr_trigger" {
   project     = var.project_id
   name        = var.trigger_name
-  description = "Cloud Build Repo trigger"
-  location    = "us-central1"
+  description = "Cloud Build GitHub App trigger managed by Terraform"
 
-  trigger_template {
-    repo_name   = var.repo_name
-    branch_name = var.branch_name   # FIXED – no regex allowed
+  # GitHub App trigger (not Cloud Build Repository v2)
+  github {
+    owner = var.github_owner
+    name  = var.github_repo
+
+    push {
+      branch = var.branch_regex
+    }
   }
 
+  # Use the cloudbuild.yaml in this repo
   filename = var.cloudbuild_yaml_path
 
+  # Pass environment into the build as a substitution
   substitutions = {
     "_ENV" = var.environment
   }
 
+  # Service account used by Cloud Build to run the steps
   service_account = var.trigger_service_account
 }
